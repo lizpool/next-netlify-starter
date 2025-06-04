961642e07653212a5e2f37dfad14612d72b42181
lizpool <alisabeth.b@gmail.com>
2025-06-03 23:24:02 -0400

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,27 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        
+
+        <script type="application/javascript">
+    document.addEventListener('kpsdk-load', function () {
+        window.KPSDK.configure([
+            protocol: 'https',
+                method: '*',
+                domain: 'verdant-llama-2f90a5.netlify.app',
+                path: '*'
+        ]);
+    });
+
+
+</script>
+<script src="https://api.example.com/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/p.js"></script>
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Helllllllooooo <code>World</code>
         </p>
       </main>
 
