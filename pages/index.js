6d0ec132723c5f1465648fc40b701b9ebd5aaf0f
lizpool <alisabeth.b@gmail.com>
2025-06-03 23:37:27 -0400

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         
 
-        <script type="application/javascript">
+
+<script type="application/javascript">
     document.addEventListener('kpsdk-load', function () {
-        window.KPSDK.configure({
-              protocol: 'https',
+        window.KPSDK.configure([
+            {
+                protocol: 'https',
                 method: '*',
                 domain: 'verdant-llama-2f90a5.netlify.app',
                 path: '*'
-});
+            }
+        ]);
     });
-
-
 </script>
+<script src="/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/p.js"></script>
+
+ 
 <script src="https://api.example.com/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/p.js"></script>
       </Head>
 
